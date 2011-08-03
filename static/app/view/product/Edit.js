6db05c987babaf3e1d9bda644342df808258662e
voidw0rd <krypton_l2@yahoo.com>
2011-08-03 19:42:48 +0300

    made lang desc textarea fields

--- static/app/view/product/Edit.js
+++ static/app/view/product/Edit.js
@@ -7,8 +7,8 @@ Ext.define('AM.view.product.Edit', {
     title : 'Edit Product',
     layout: 'fit',
     autoShow: true,
-    height: 640,
-    width: 380,
+    height: 680,
+    width: 400,
 
     initComponent: function() {
         this.items = [
@@ -21,92 +21,115 @@ Ext.define('AM.view.product.Edit', {
                 items: [
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'cod',
                         fieldLabel: 'Code'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'denumirePlic',
                         fieldLabel: 'Denumire plic'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'denumireOferta',
                         fieldLabel: 'Denumire oferta'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'denumireLatina',
                         fieldLabel: 'Denumire latina'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'soi',
                         fieldLabel: 'Soi'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'photoCode',
                         fieldLabel: 'Photo Code'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'namesLanguages',
                         fieldLabel: 'Names-languages'
                     },
                     {
-                        xtype: 'textfield',
+                        xtype: 'textarea',
+                        width: 370,
+                        height: 40,
                         name : 'roDesc',
-                        fieldLabel: 'RO'
+                        fieldLabel: 'RO',
                     },
                     {
-                        xtype: 'textfield',
+                        xtype: 'textarea',
+                        width: 370,
+                        height: 40,
                         name : 'enDesc',
                         fieldLabel: 'EN'
                     },
                     {
-                        xtype: 'textfield',
+                        xtype: 'textarea',
+                        width: 370,
+                        height: 40,
                         name : 'huDesc',
                         fieldLabel: 'HU'
                     },
                     {
-                        xtype: 'textfield',
+                        xtype: 'textarea',
+                        width: 370,
+                        height: 40,
                         name : 'sbDesc',
                         fieldLabel: 'SB'
                     },
                     {
-                        xtype: 'textfield',
+                        xtype: 'textarea',
+                        width: 370,
+                        height: 40,
                         name : 'ruDesc',
                         fieldLabel: 'RU'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'stage1',
                         fieldLabel: 'Stage 1'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'stage2',
                         fieldLabel: 'Stage 2'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'stage3',
                         fieldLabel: 'Stage 3'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'stage4',
                         fieldLabel: 'Stage 4'
                     },
                     {
                         xtype: 'textfield',
+                        width: 370,
                         name : 'stage5',
                         fieldLabel: 'Stage 5'
                     },
                     {
                         xtype: 'textfield',
                         name : 'category',
+                        width: 370,
                         fieldLabel: 'Category'
                     }
                 ]
