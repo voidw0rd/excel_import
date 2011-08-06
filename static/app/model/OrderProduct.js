c015f4676f5ab2015bb9796aea3ae64dfacf7a34
vlad <vladisac@gmail.com>
2011-08-06 14:01:20 +0300

    scos asocieri din modelele extjs

--- static/app/model/OrderProduct.js
+++ static/app/model/OrderProduct.js
@@ -1,5 +1,4 @@
 Ext.define('AM.model.OrderProduct', {
     extend: 'Ext.data.Model',
     fields: ['id',  'order_id', 'product_id', 'cod', 'name', 'quantity'],
-    belongsTo: {model:'AM.model.Order'}
 });
