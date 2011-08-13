7b66ccabc475860feb05a4e2604f8b6f67654062
voidw0rd <krypton_l2@yahoo.com>
2011-08-13 14:57:24 +0300

    orderProducts are referinta doar la product_id

--- grid/models.py
+++ grid/models.py
@@ -30,28 +30,6 @@ class OrderProduct(models.Model):
     quantity = models.IntegerField()
     order = models.ForeignKey("Orders", related_name = "order_products")
     note = models.TextField()
-    # ----------------------------------------------------------------------#
-    # a product can change but a product from an order should never change  #
-    
-    cod = models.CharField(max_length = 10)
-    denumirePlic = models.CharField(max_length = 50)
-    denumireOferta = models.CharField(max_length = 50)
-    denumireLatina = models.CharField(max_length = 50)
-    soi = models.CharField(max_length = 50)
-    photoCode = models.CharField(max_length = 20)
-    namesLanguages = models.CharField(max_length = 50)
-    roDesc = models.TextField()
-    enDesc = models.TextField()
-    huDesc = models.TextField()
-    sbDesc = models.TextField()
-    ruDesc = models.TextField()
-    stage1 = models.CharField(max_length = 10, blank = True)
-    stage2 = models.CharField(max_length = 10)
-    stage3 = models.CharField(max_length = 10)
-    stage4 = models.CharField(max_length = 10)
-    stage5 = models.CharField(max_length = 10)
-    category = models.CharField(max_length = 1)
-    
 
 
 class OrderStatuses(models.Model):
